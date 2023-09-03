3e73ac411eab3cae4f96c5f26f67229f72ab6fc4
wikijoseph <wikijoseph8@gmail.com>
2023-09-03 22:28:37 +0800

    a

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*---------------------------------------------
+Template name:  tf-quickstart || html template
+Version:        1.0
+更多精品模板：http://www.bootstrapmb.com
+
+NOTE:
+------
+Please DO NOT EDIT THIS JS, you may need to use "custom.js" file for writing your custom js.
+We may release future updates so it will overwrite this file. it's better and safer to use "custom.js".
+
+[Table of Content]
+----------------------------------------------*/
+
+(function ($) {})(jQuery);
